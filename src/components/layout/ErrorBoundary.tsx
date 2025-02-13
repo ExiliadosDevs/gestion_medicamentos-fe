137b75deeda853ad3b110b25d8b21503dd4c7dcd
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-02-13 13:02:46 -0300

    feat: add ErrorBoundaryComponent for error handling

---
+++ src/components/layout/ErrorBoundary.tsx
@@ -0,0 +1,38 @@
+import { Component, ReactNode } from "react";
+
+interface Props {
+    children: ReactNode;
+    fallback: ReactNode;
+    resetCondition?: any;
+}
+
+interface State {
+    hasError: boolean;
+    resetCondition?: any;
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = { hasError: false, resetCondition: this.props.resetCondition };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+        return { hasError: true };
+    }
+
+    static getDerivedStateFromProps(props: Props, state: State){
+        if(props.resetCondition !== state.resetCondition) {
+            return { hasError: false, resetCondition: props.resetCondition};
+        }
+        return null;
+    }
+
+    render() {
+        if(this.state.hasError){
+            return this.props.fallback;
+        }
+        return this.props.children;
+    }
+}
